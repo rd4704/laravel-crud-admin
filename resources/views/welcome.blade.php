0fa8bf0ce04f4022c409868bc58035e16df6c488
Maks <mahmoud.adel@gmail.com>
2017-03-06 17:25:44 +0800

    fix secure url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 
     <div class="links">
         <a id="contact-me-link" class="contact-link" href="{{url('/')}}">Contact me</a>
-        <a id="ping-challenge-link" href="{{url('/ping')}}">Ping Challenge Link</a>
-        <a id="repository-link" href="{{url('/readme')}}">Read me</a>
+        <a id="ping-challenge-link" href="{{secure_url("/ping")}}">Ping Challenge Link</a>
+        <a id="repository-link" href="{{secure_url("/readme")}}">Read me</a>
     </div>
 @stop

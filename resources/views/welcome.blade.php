4f8d72c7ddc3383e1b56b247bf454d59aa2a576f
yazan <yazan.bakour@ipricegroup.com>
2017-03-06 15:24:22 +0800

    add id

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,8 @@
                     Hello Yose
                 </div>
                 <div class="links">
-                    <a id="contact-me-link" class="contact-link" href="{{url('/')}}">Contact me</a>
+                    <a id="contact-me-link" href="{{url('/')}}">Contact me</a>
+                    <a id="ping-challenge-link" href="{{url('/ping')}}">Ping Challenge Link</a>
                 </div>
             </div>
         </div>

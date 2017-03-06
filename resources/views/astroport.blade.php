890c41df48450bf8b3db9563aaf31845ab9cbe79
Rahul <rd4704@gmail.com>
2017-03-06 16:49:38 +0800

    added ships

--- resources/views/astroport.blade.php
+++ resources/views/astroport.blade.php
@@ -4,4 +4,11 @@
     <div id="astroport-name" class="title m-b-md">
         Artisans
     </div>
+    <div class="links">
+        @for($j=1; $j<=3; $j++)
+            <div id="gate-{{ $j }}">
+                <div id="ship-{{ $j }}">Ship {{ $j }}</div>
+            </div>
+        @endfor
+    </div>
 @stop

f135d7a8fc301acd84ce9bf49c410ea8f78f5dde
yazan <yazan.bakour@ipricegroup.com>
2017-03-07 09:51:26 +0800

    change url to secure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="links">
-        <a id="contact-me-link" class="contact-link" href="{{url('/contact')}}">Contact me</a>
+        <a id="contact-me-link" class="contact-link" href="{{secure_url('/contact')}}">Contact me</a>
         <a id="ping-challenge-link" href="{{secure_url("/ping")}}">Ping Challenge Link</a>
         <a id="repository-link" href="{{secure_url("/readme")}}">Read me</a>
     </div>

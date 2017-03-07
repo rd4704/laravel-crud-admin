43a363379d0d0f27d7bfcee56c3736001d02bf5a
Rahul <rd4704@gmail.com>
2017-03-07 16:13:29 +0800

    Minesweeper Safe Cell

--- resources/views/minesweeper.blade.php
+++ resources/views/minesweeper.blade.php
@@ -22,9 +22,9 @@
         var dummyData = [
             ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
             ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb' , 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-            ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
+            ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty', 'empty'],
+            ['empty', 'empty', 'empty', 'empty', 'empty', 'bomb', 'empty', 'empty'],
+            ['empty', 'empty', 'empty', 'empty', 'bomb', 'empty', 'empty', 'empty'],
             ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
             ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
             ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
@@ -34,16 +34,42 @@
 
         function load() {
             data = (document.grid !== undefined) ? document.grid : dummyData;
-            for (var i=0; i<data.length; i++) {
-                for (var j=0; j<data[i].length; j++) {
-                    $('#cell-' + (i+1) + 'x' + (j+1)).text(data[i][j]);
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < data[i].length; j++) {
+                    $('#cell-' + (i + 1) + 'x' + (j + 1)).text(data[i][j]);
                 }
             }
         }
 
-        $( ".Grid-cell" ).on( "click", function() {
-            if($(this).text() === 'bomb') {
+        $(".Grid-cell").on("click", function () {
+            if ($(this).text() === 'bomb') {
                 $(this).addClass('lost');
+            }
+
+            var arrToCheck = [
+                [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
+            ];
+
+            if ($(this).text() === 'empty') {
+                var cell = $(this).attr('id').split('-')[1];
+
+                var row_col = cell.split('x');
+                var row = row_col[0];
+                var col = row_col[1];
+
+                var result = 0;
+                for (var i = 0; i < arrToCheck.length; i++) {
+                    var nearestCellInView = $('#cell-' + (parseInt(row) + parseInt(arrToCheck[i][0])) + 'x' + (parseInt(col) + parseInt(arrToCheck[i][1])));
+
+                    if (nearestCellInView !== undefined && nearestCellInView.text() === 'bomb') {
+                        result +=1;
+                    }
+                }
+                $(this).addClass('safe');
+
+                if (result !== 0) {
+                    $(this).text(result);
+                }
             }
         });
 
